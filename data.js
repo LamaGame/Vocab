f35c275449ce7cb226b20da599ee1be11b0e9111
Leo G. <95503088+LamaGame@users.noreply.github.com>
2025-03-10 17:43:13 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+const vocabulary = {
+    "Einheit 1": [
+        { german: "Haus", french: "maison" },
+        { german: "Auto", french: "voiture" },
+        { german: "Baum", french: "arbre" }
+    ],
+    "Einheit 2": [
+        { german: "Tisch", french: "table" },
+        { german: "Stuhl", french: "chaise" },
+        { german: "Fenster", french: "fenêtre" }
+    ]
+};
