35814a4480e908535dff7e88cfb92f960964485c
Leo G. <95503088+LamaGame@users.noreply.github.com>
2025-03-13 12:41:50 +0100

    Update data.js

--- French-Vocab/data.js
+++ French-Vocab/data.js
@@ -44,14 +44,14 @@ const vocabulary = {
         },
         { 
             french: "un effet", 
-            german: "eine Wirkung; ein Effekt", 
+            german: "eine Wirkung, ein Effekt", 
             noteFrench: "", 
             noteGerman: "", 
             exampleFrench: "Les effets d'une alimentation sans sucre sont extraordinaires." 
         },
         { 
             french: "mettre qn en colère", 
-            german: "jdn ärgern; jdn wütend machen", 
+            german: "jdn ärgern, jdn wütend machen", 
             noteFrench: "", 
             noteGerman: "", 
             exampleFrench: "Les critiques me mettent en colère." 
@@ -65,7 +65,7 @@ const vocabulary = {
         },
         { 
             french: "surprendre qn", 
-            german: "jdn überraschen; jdn erstaunen", 
+            german: "jdn überraschen, jdn erstaunen", 
             noteFrench: "", 
             noteGerman: "wird wie prendre konjugiert", 
             exampleFrench: "J'adore surprendre mes amis." 
@@ -78,46 +78,116 @@ const vocabulary = {
             exampleFrench: "Tu veux un thé ou un café ? Comme tu veux, ça m'est égal." 
         },
         { 
+            french: "influencer qn", 
+            german: "jdn beeinflussen", 
+            noteFrench: "", 
+            noteGerman: "", 
+            exampleFrench: "Mes parents essaient tout le temps de m'influencer." 
+        },
+        { 
             french: "faire", 
-            german: "jdn etw. tun lassen; jdn dazu bringen, etw. zu tun; jdn dazu zwingen, etw. zu tun", 
+            german: "jdn etw. tun lassen; jdn dazu bringen/zwingen, etw. zu tun", 
             noteFrench: "+ inf.", 
             noteGerman: "", 
             exampleFrench: "Cette année, j'aime bien mes cours d'histoire. Ils me font réfléchir." 
         },
         { 
+            french: "rire", 
+            german: "lachen", 
+            noteFrench: "", 
+            noteGerman: "", 
+            exampleFrench: "Avec mes amis, on rit beaucoup." 
+        },
+        { 
+            french: "pleurer", 
+            german: "weinen", 
+            noteFrench: "", 
+            noteGerman: "", 
+            exampleFrench: "Pourquoi est-ce que tu pleures ?" 
+        },
+        { 
             french: "rendre qn/qc", 
-            german: "jdn/etw. (+ Adj.) machen", 
+            german: "jdn/etw. machen", 
             noteFrench: "+ adj.", 
             noteGerman: "als Wirkung; wird wie attendre konjugiert", 
             exampleFrench: "Quand mes amis sont contents, ça me rend heureux !" 
         },
         { 
-            french: "sentir qc", 
-            german: "etw. riechen; etw. schmecken", 
+            french: "espérer que", 
+            german: "hoffen, dass", 
             noteFrench: "", 
-            noteGerman: "wird wie sortir konjugiert", 
-            exampleFrench: "Ça sent bon." 
+            noteGerman: "wird wie préférer konjugiert", 
+            exampleFrench: "J'espère qu'on va bientôt arriver." 
         },
         { 
             french: "moderniser qc", 
-            german: "etw. modernisieren; etw. modern umgestalten", 
-            noteFrench: "→ moderne", 
+            german: "etw. modernisieren, etw. modern umgestalten", 
+            noteFrench: "", 
             noteGerman: "", 
-            exampleFrench: "" 
+            exampleFrench: "→ moderne" 
         },
         { 
             french: "arrêter de", 
-            german: "aufhören, etw. zu tun", 
-            noteFrench: "+ inf.", 
+            german: "aufhören etw. zu tun", 
+            noteFrench: " + inf.", 
             noteGerman: "", 
             exampleFrench: "Arrête de dessiner en classe !" 
         },
         { 
+            french: "faux/fausse", 
+            german: "falsch", 
+            noteFrench: "", 
+            noteGerman: "", 
+            exampleFrench: "≠ vrai" 
+        },
+        { 
             french: "libre", 
             german: "frei", 
-            noteFrench: "→ la liberté", 
+            noteFrench: "", 
             noteGerman: "", 
-            exampleFrench: "" 
+            exampleFrench: "→ la liberté" 
+        },
+        { 
+            french: "la mobylette", 
+            german: "das Mofa", 
+            noteFrench: "", 
+            noteGerman: "", 
+            exampleFrench: "Ma mobylette est cassée, je dois aller au travail à pied." 
+        },
+        { 
+            french: "ailleurs", 
+            german: "anderswo, woandershin", 
+            noteFrench: "", 
+            noteGerman: "", 
+            exampleFrench: "Est-ce que tu voudrais vivre ailleurs ?" 
+        },
+        { 
+            french: "être à table", 
+            german: "am (Ess-)Tisch sitzen, bei Tisch sein", 
+            noteFrench: "", 
+            noteGerman: "", 
+            exampleFrench: "Le soir, quand on est à table, on raconte notre journée !" 
+        },
+        { 
+            french: "stressé/stressée", 
+            german: "gestresst", 
+            noteFrench: "", 
+            noteGerman: "", 
+            exampleFrench: "Les gens stressés dorment mal." 
+        },
+        { 
+            french: "sentir qc", 
+            german: "etw. riechen, auch: etw. schmecken", 
+            noteFrench: "", 
+            noteGerman: "wird wie sortir konjugiert", 
+            exampleFrench: "ça sent" 
+        },
+        { 
+            french: "triste", 
+            german: "traurig", 
+            noteFrench: "", 
+            noteGerman: "", 
+            exampleFrench: "Je n'aime pas les histoires tristes." 
         },
         { 
             french: "agressif/agressive", 
@@ -125,6 +195,13 @@ const vocabulary = {
             noteFrench: "", 
             noteGerman: "", 
             exampleFrench: "Ce chien est plutôt agressif." 
+        },
+        { 
+            french: "donner envie (à qn) de", 
+            german: "(jdm) Lust machen etw. zu tun", 
+            noteFrench: "→ avoir envie de + inf.", 
+            noteGerman: "", 
+            exampleFrench: "Ça me donne envie de danser !" 
         },
         { 
             french: "empêcher qn/qc de", 
@@ -136,9 +213,16 @@ const vocabulary = {
         { 
             french: "mentir", 
             german: "lügen", 
-            noteFrench: "à qn", 
+            noteFrench: "(à qn)", 
             noteGerman: "wird wie sortir konjugiert", 
             exampleFrench: "Non, ce n'est pas vrai, tu mens !" 
+        },
+        { 
+            french: "se moquer de qn/qc", 
+            german: "sich über jdn/etw. lustig machen", 
+            noteFrench: "", 
+            noteGerman: "", 
+            exampleFrench: "Ne te moque pas de moi !" 
         },
         { 
             french: "les quatre coins", 
@@ -148,15 +232,57 @@ const vocabulary = {
             exampleFrench: "Ces bateaux viennent des quatre coins du monde." 
         },
         { 
+            french: "voici", 
+            german: "hier ist/sind", 
+            noteFrench: "= voilà", 
+            noteGerman: "", 
+            exampleFrench: "" 
+        },
+        { 
+            french: "le vent", 
+            german: "der Wind", 
+            noteFrench: "", 
+            noteGerman: "", 
+            exampleFrench: "Ce matin, le vent est assez froid." 
+        },
+        { 
+            french: "prochain/prochaine", 
+            german: "nächster, nächste, nächstes", 
+            noteFrench: "", 
+            noteGerman: "", 
+            exampleFrench: "Samedi prochain, nous partons en vacances." 
+        },
+        { 
+            french: "spécial/spéciale/spéciaux/spéciales", 
+            german: "besonders, speziell", 
+            noteFrench: "→ se spécialiser", 
+            noteGerman: "", 
+            exampleFrench: "" 
+        },
+        { 
+            french: "le produit", 
+            german: "das Produkt, die Ware", 
+            noteFrench: "", 
+            noteGerman: "", 
+            exampleFrench: "Ce sont des produits de la région." 
+        },
+        { 
+            french: "produire qc", 
+            german: "etw. produzieren, herstellen, erzeugen", 
+            noteFrench: "→ le produit", 
+            noteGerman: "wird wie construire konjugiert", 
+            exampleFrench: "Dans notre région, on produit beaucoup de fromages." 
+        },
+        { 
             french: "se plaindre (de qn/qc)", 
             german: "sich (über jdn/etw.) beschweren", 
             noteFrench: "", 
-            noteGerman: "wird wie craindre konjugiert", 
+            noteGerman: "Verbes, p. 141", 
             exampleFrench: "Les habitants se plaignent du bruit des voitures." 
         },
         { 
             french: "un immeuble", 
-            german: "ein Haus; ein Wohnhaus", 
+            german: "ein (Wohn-)Haus", 
             noteFrench: "", 
             noteGerman: "", 
             exampleFrench: "J'habite dans un immeuble." 
@@ -164,7 +290,7 @@ const vocabulary = {
         { 
             french: "pauvre", 
             german: "arm", 
-            noteFrench: "↔ riche", 
+            noteFrench: "", 
             noteGerman: "", 
             exampleFrench: "C'est une région assez pauvre." 
         },
@@ -176,8 +302,22 @@ const vocabulary = {
             exampleFrench: "" 
         },
         { 
+            french: "la réputation", 
+            german: "der Ruf, das Ansehen", 
+            noteFrench: "", 
+            noteGerman: "", 
+            exampleFrench: "Ce collège a une bonne réputation." 
+        },
+        { 
+            french: "tous/toutes", 
+            german: "alle", 
+            noteFrench: "", 
+            noteGerman: "", 
+            exampleFrench: "Mes amis habitent tous à Lille !" 
+        },
+        { 
             french: "un étage", 
-            german: "ein Stockwerk; eine Etage", 
+            german: "ein Stockwerk, eine Etage", 
             noteFrench: "", 
             noteGerman: "", 
             exampleFrench: "On habite au premier étage." 
